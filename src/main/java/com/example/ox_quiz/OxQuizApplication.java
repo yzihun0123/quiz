328053b13af574502686c4d7292c2a285e43f55f
yzihn0123 <zihoon0123@gmail.com>
2026-05-18 14:54:32 +0900

    first commit

---
+++ src/main/java/com/example/ox_quiz/OxQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.ox_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxQuizApplication.class, args);
+	}
+
+}
